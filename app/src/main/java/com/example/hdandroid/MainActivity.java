2187314608636cbab73aeb38ffc870384aa6e0bf
Ada Wong <adawong@Adas-MacBook-Pro.local>
2019-10-24 13:55:29 -0400

    update main activity

--- app/src/main/java/com/example/hdandroid/MainActivity.java
+++ app/src/main/java/com/example/hdandroid/MainActivity.java
@@ -2,13 +2,155 @@ package com.example.hdandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.os.Handler;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView countdown;
+    Button checkInButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        countdown = findViewById(R.id.countdown);
+        checkInButton = findViewById(R.id.checkInButton);
+
     }
+
+    public Date StringToDate(String s){
+
+        Date result = null;
+        try{
+            SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            result  = dateFormat.parse(s);
+        }
+
+        catch(ParseException e){
+            e.printStackTrace();
+
+        }
+        return result ;
+    }
+
+    /**
+     * Returns a formatted string containing the amount of time (days, hours,
+     * minutes, seconds) between the current time and the specified future date.
+     *
+     * @param context
+     * @param futureDate
+     * @return
+     */
+    public static CharSequence getCountdownText(Context context, Date futureDate) {
+        StringBuilder countdownText = new StringBuilder();
+
+        // Calculate the time between now and the future date.
+        long timeRemaining = futureDate.getTime() - new Date().getTime();
+
+        // If there is no time between (ie. the date is now or in the past), do nothing
+        if (timeRemaining > 0) {
+            Resources resources = context.getResources();
+
+            // Calculate the days/hours/minutes/seconds within the time difference.
+            //
+            // It's important to subtract the value from the total time remaining after each is calculated.
+            // For example, if we didn't do this and the time was 25 hours from now,
+            // we would get `1 day, 25 hours`.
+            int days = (int) TimeUnit.MILLISECONDS.toDays(timeRemaining);
+            timeRemaining -= TimeUnit.DAYS.toMillis(days);
+            int hours = (int) TimeUnit.MILLISECONDS.toHours(timeRemaining);
+            timeRemaining -= TimeUnit.HOURS.toMillis(hours);
+            int minutes = (int) TimeUnit.MILLISECONDS.toMinutes(timeRemaining);
+            timeRemaining -= TimeUnit.MINUTES.toMillis(minutes);
+            int seconds = (int) TimeUnit.MILLISECONDS.toSeconds(timeRemaining);
+
+            // For each time unit, add the quantity string to the output, with a space.
+            if (days > 0) {
+                countdownText.append(resources.getQuantityString(R.plurals.days, days, days));
+                countdownText.append(" ");
+            }
+            if (days > 0 || hours > 0) {
+                countdownText.append(resources.getQuantityString(R.plurals.hours, hours, hours));
+                countdownText.append(" ");
+            }
+            if (days > 0 || hours > 0 || minutes > 0) {
+                countdownText.append(resources.getQuantityString(R.plurals.minutes, minutes, minutes));
+                countdownText.append(" ");
+            }
+            if (days > 0 || hours > 0 || minutes > 0 || seconds > 0) {
+                countdownText.append(resources.getQuantityString(R.plurals.seconds, seconds, seconds));
+                countdownText.append(" ");
+            }
+        }
+
+        return countdownText.toString();
+    }
+
+
+
+    /**
+     * The time (in ms) interval to update the countdown TextView.
+     */
+    private static final int COUNTDOWN_UPDATE_INTERVAL = 500;
+
+    private Handler countdownHandler;
+
+    /**
+     * Stops the  countdown timer.
+     */
+    private void stopCountdown() {
+        if (countdownHandler != null) {
+            countdownHandler.removeCallbacks(updateCountdown);
+            countdownHandler = null;
+        }
+    }
+
+    /**
+     * (Optionally stops) and starts the countdown timer.
+     */
+    private void startCountdown() {
+        stopCountdown();
+
+        countdownHandler = new Handler();
+        updateCountdown.run();
+    }
+
+    /**
+     * Updates the countdown.
+     */
+    private Runnable updateCountdown = new Runnable() {
+        @Override
+        public void run() {
+            try {
+                Date futureDate = StringToDate("2019-11-02 9:00:00");
+                countdown.setText(getCountdownText(countdown.getContext(), futureDate));
+            } finally {
+                countdownHandler.postDelayed(updateCountdown, COUNTDOWN_UPDATE_INTERVAL);
+            }
+        }
+    };
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        startCountdown();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        stopCountdown();
+    }
+
 }
--- app/src/main/java/com/example/hdandroid/TabActivity.java
+++ app/src/main/java/com/example/hdandroid/TabActivity.java
@@ -22,7 +22,7 @@ public class TabActivity extends AppCompatActivity {
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
                 .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        NavController navController = Navigation.findNavController(this, R.id.nav_controller_view_tag);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
     }
